1f856a728ca447f69afe3c8513dfc9e04e639ccc
Malachai <xogmldntmd@naver.com>
2022-04-11 17:04:06 +0900

    added SecurityConfig, templates and layouts, routers

---
+++ src/main/java/com/cos/security/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cos.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                //도메인에 따른 권한 제한 및 배정
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+
+                .and()
+                .formLogin()
+                    .loginPage("/login");
+    }
+}
--- src/main/java/com/cos/security/controller/IndexController.java
+++ src/main/java/com/cos/security/controller/IndexController.java
@@ -2,13 +2,44 @@ package com.cos.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     @GetMapping({"", "/"})
     public String index() {
+        //Mustache의 기본 폴더: src/main/resources/
+        //Spring에서 지원하는 mustache에 설정되는 뷰리졸버:  prefix-/templates/, suffix-.mustache
         return "index";
     }
 
+    //인덱스 컨트롤러 명명법 전략
+    //url과 탬플릿의 이름이 같으면 circular path excetion을 일으킴
+    // "/"를 index로 명명하며 이후 도메인을 "-" 로 순차적으로 연결
+    @GetMapping("/user")
+    public String user() {
+        return "index-user";
+    }
+    @GetMapping("/admin")
+    public String admin() {
+        return "index-admin";
+    }
+    @GetMapping("/manager")
+    public String manager() {
+        return "index-manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "index-login";
+    }
+    @GetMapping("/join")
+    public String join() {
+        return "index-join";
+    }
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입이 완료되었습니다";
+    }
 }
