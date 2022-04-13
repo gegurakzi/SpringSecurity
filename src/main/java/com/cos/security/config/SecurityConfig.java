6777c3124306427a24eea3f4409830a07cddad9b
Malachai <xogmldntmd@naver.com>
2022-04-13 18:05:57 +0900

    main branch hotfix for SecurityConfig "ROLE_" prefix

--- src/main/java/com/cos/security/config/SecurityConfig.java
+++ src/main/java/com/cos/security/config/SecurityConfig.java
@@ -23,9 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 //도메인에 따른 권한 제한 및 배정
                 .antMatchers("/user/**").authenticated()
-                .antMatchers("/login/**", "/register/**").not().authenticated()
+                // DB에 role은 prefix로 ROLE_을 가져야만 Security에서 인식한다. hasRole은 ROLE_을 자동으로 prefix로 붙여 사용자의 권한을 체크한다.
                 .antMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
                 .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/login/**", "/register/**").not().authenticated()
+
                 .anyRequest().permitAll()
 
                 .and()
